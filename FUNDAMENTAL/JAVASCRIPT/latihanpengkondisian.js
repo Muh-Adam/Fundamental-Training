bff2c6ac087d77f9aac5ee0e2b63bfb28688ac44
Muh-Adam <adamyasin258@gmail.com>
2025-05-18 21:57:08 +0800

    first commit

---
+++ FUNDAMENTAL/JAVASCRIPT/latihanpengkondisian.js
@@ -0,0 +1,11 @@
+// IF ELSE
+
+var angkot = 1
+
+for(angkot ; angkot<=10 ; angkot++){
+    if(angkot <= 6){
+        console.log('Angkot No.' + angkot + ' Beroperasi dengan baik.');
+        }else{
+            console.log('Angkot No.' + angkot + ' Tidak beroperasi dengan baik.')
+        }
+}
