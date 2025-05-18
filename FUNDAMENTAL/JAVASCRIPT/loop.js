ffd7a0063362f88d1ed6a2bb936994ecd2762f99
Muh-Adam <adamyasin258@gmail.com>
2025-05-18 16:58:49 +0800

    first commit

---
+++ FUNDAMENTAL/JAVASCRIPT/loop.js
@@ -0,0 +1,41 @@
+// WHILE
+
+//  while(true){
+//     console.log('Hello World');
+// }
+// COMMAND DIATAS AKAN MENGHASILKAN INFINITE LOOP SELAMA NILAI DI DALAM WHILE BERNILAI TRUE
+
+
+// JENIS LOOP DIBAWAH BISA DIHENTIKAN OLEH USER DENGAN MENEKAN TOMBOL CANCEL, SEHINGGA NILAI DALAM VARIABEL ULANG BERUBAH MENJADI FALSE
+// var ulang = true
+// while(ulang){
+//     console.log('Hello World');
+//     ulang = confirm('ulang lagi?');
+// }
+
+
+// JENIS LOOP DIBAWAH DIHENTIKAN OLEH SISTEM, LOOP AKAN BERHENTI JIKA NILAI VARIABEL MENJAPAI NILAI YANG TELAH DITENTUKAN
+
+// var nilai = 1
+// while(nilai<=15){
+//     console.log('Hello Noir');
+//     nilai++;
+// }
+
+// var nilai = 20
+// while(nilai>=1){
+//     console.log('Hello Noir');
+//     nilai--;
+// }
+
+// var nilai = 1
+// while(nilai<=100){
+//     console.log('Hello Noir '+ nilai + 'x');
+//     nilai++;
+// }
+
+var angkot = 1
+while(angkot <= 10){
+    console.log('Angkot No. '+ angkot + ' Beroperasi dengan baik.');
+    angkot++;
+}
