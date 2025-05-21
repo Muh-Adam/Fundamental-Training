23ccc373d572b373d311bb14d63ff82f3a442547
Muh-Adam <adamyasin258@gmail.com>
2025-05-21 21:15:50 +0800

    add manipulasi array

---
+++ FUNDAMENTAL/JAVASCRIPT/manipulasiarray.js
@@ -0,0 +1,32 @@
+// MANIPULASI ARRAY
+
+// 1.MENAMBAHKAN ARRAY
+// APABILA MELEWATKAN SATU KEY, MAKA NILAI DARI KEY YANG DILEWATKAN AKAN TETAP TERHITUNG DAN NILAINYA MENJADI UNDEFINED
+var arr = []
+arr [0] = 'scorpio'
+arr [1] = 'leo'
+arr [2] = 'aries'
+arr [3] = 'pisces'
+arr [4] = 'sagitarius'
+
+console.log(arr[4])
+
+// 2. MENGHAPUS ARRAY
+var arr1 = ['scorpio', 'leo', 'capricorn', 'aries', 'pisces']
+arr1 [1] = undefined
+
+console.log(arr1)
+
+// 3. MENAMPILKAN ISI ARRAY
+var arr2 = ['scorpio', 'leo', 'capricorn', 'aries', 'pisces']
+console.log(arr2)
+
+// ATAU
+
+// for(var i = 0 ; i < arr2.length ; i++){
+//     console.log(arr[i])
+// }
+
+for(var i = 0 ; i < arr2.length ; i++){
+    console.log('Zodiak kamu adalah No.' + [i+1] + ' yaitu ' + arr2[i])
+}
