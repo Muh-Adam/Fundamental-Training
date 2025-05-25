5e78ada780fe6fa617c1812af07e7ef1f56399d4
Muh-Adam <adamyasin258@gmail.com>
2025-05-25 21:15:51 +0800

    add DOM Selector

---
+++ FUNDAMENTAL/JAVASCRIPT/DOM/DOM.js
@@ -0,0 +1,12 @@
+// Document Object Model (DOM)
+// DOM adalah antarmuka pemrograman untuk HTML yang merepresentasikan halaman web, sehingga program dapat mengubah dan memanipulasi strukturnya.
+
+// DOM
+// Antarmuka pemrograman berbasis objek yang merepresentasikan dokumen web.
+// DOM membuat seluruh komponen dari halaman web dapat diakses dan dimanipulasi.
+
+// KOMPONEN
+// Elemen HTML.
+// Attribut.
+// Text.
+// dll.
---
+++ FUNDAMENTAL/JAVASCRIPT/DOM/DOMSelector.js
@@ -0,0 +1,11 @@
+// DOM Selection Method                                 ===> Hasil
+// getElementById()                                     ===> Element
+// getElemenetsByTagName()                              ===> HTMLCollection
+// getElementsByClassName()                             ===> HTMLCollection
+// querrySelector()                                     ===> Element
+// querrySelectorAll()                                  ===> NodeList
+
+
+// getElementById()
+const judul = getElementById ('judul')
+judul.style.color='red'
