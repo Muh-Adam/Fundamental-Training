1605e63ec0fb2cd26ec7602adff7d496c33a550e
Muh-Adam <adamyasin258@gmail.com>
2025-05-28 21:15:26 +0800

    update DOMManipulation

--- FUNDAMENTAL/JAVASCRIPT/DOM/DOMManipulation.js
+++ FUNDAMENTAL/JAVASCRIPT/DOM/DOMManipulation.js
@@ -30,9 +30,57 @@
 // element.getAttribute()
 // element.setAttribute() 
 // element.removeAttribute()
+// setAttribute('attribute', 'value');
 
 const h1 = document.getElementsByTagName('h1')[0];
 h1.setAttribute('name', 'Muhammad Adam');
 
-const sectionA = document.querySelector('section#a');
-sectionA.removeAttribute('href')
+const sectionA = document.querySelector('section#a a');
+sectionA.removeAttribute('href');
+
+
+
+// element.classList.add()              ===> Menambahkan class
+// element.classList.remove()           ===> Menghapus class
+// element.classList.toggle()           ===> Menambahkan / Menghapus class pada suatu elemen, tergantung apakah pada elemen tersebut sudah ada class tersebut atau belum
+// element.classList.item()             ===> Mencari class yang ada dalam elemen berdasarkan index
+// element.classList.contains()         ===> Mencari class yang ada pada suatu elemen berdasarkan value / nama class
+// element.classList.replace()          ===> Mengganti class yang ada dalam suatu elemen menggunakan class baru
+
+
+const body = document.body;
+body.classList.toggle('biru-muda');
+body.classList.toggle('biru-muda');
+
+
+
+
+
+
+
+
+
+
+// Manipulasi Node
+
+// Method
+// document.createElement()
+// document.createTextNode()
+// node.appendChild()
+// node.insertBefore()
+// parentNode.removeChild()
+// parentNode.replaceChild()
+// ......
+
+
+// Buat Element Baru
+const pBaru = document.createElement('p');
+const textpBaru = document.createTextNode('Paragraph Baru');
+
+// Menyimpan text kedalam Element
+pBaru.appendChild(textpBaru);
+
+// Simpan pBaru diakhir section A
+const sectA = document.getElementById('a');
+
+sectA.appendChild(pBaru);
