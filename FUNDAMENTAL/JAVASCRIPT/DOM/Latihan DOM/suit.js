dcdcec2f51a2065021e4b96cf5483ddc20ded8fc
Muh-Adam <adamyasin258@gmail.com>
2025-06-01 20:58:49 +0800

    update

--- FUNDAMENTAL/JAVASCRIPT/DOM/Latihan DOM/suit.js
+++ FUNDAMENTAL/JAVASCRIPT/DOM/Latihan DOM/suit.js
@@ -12,8 +12,9 @@ function getPilihanComputer(){
 
 function getHasil(comp, player){
     if(comp == player)return 'Seri!'
-    if(player == 'gunting')return (comp == 'batu')? 'Menang': 'Kalah';
-    if(player == 'batu')return (comp == 'kertas')? 'Kalah': 'Menang';
+    if(player == 'gunting')return (comp == 'kertas')? 'Menang!': 'Kalah!';
+    if(player == 'batu')return (comp == 'gunting')? 'Menang!' :'Kalah!';
+    if(player == 'kertas')return (comp == 'batu')? 'Menang!' :'Kalah!';
 }
 
 
@@ -22,12 +23,94 @@ const pGunting = document.querySelector('.gunting');
 const pBatu = document.querySelector('.batu');
 const pKertas = document.querySelector('.kertas');
 
-// Membuat event
-pGunting.addEventListener('click', function(){
+// // Membuat event
+// pGunting.addEventListener('click', function(){
+//     // Menentukan pilihan computer dan player serta hasil
+//     const pilihanComputer = getPilihanComputer();
+//     const pilihanPlayer = pGunting.className;
+//     const hasil = getHasil(pilihanComputer, pilihanPlayer);
+
+//     // Mengubah gambar computer
+//     const imgComputer = document.querySelector('.img-computer');
+//     imgComputer.setAttribute('src', 'img/' + pilihanComputer + '.jpg');
+
+//     // Menuliskan Hasil
+//     const info = document.querySelector('.info');
+//     info.innerHTML = hasil
+// });
+
+// pBatu.addEventListener('click', function(){
+//     // Menentukan pilihan computer dan player serta hasil
+//     const pilihanComputer = getPilihanComputer();
+//     const pilihanPlayer = pBatu.className;
+//     const hasil = getHasil(pilihanComputer, pilihanPlayer);
+
+//     // Mengubah gambar computer
+//     const imgComputer = document.querySelector('.img-computer');
+//     imgComputer.setAttribute('src', 'img/' + pilihanComputer + '.jpg');
+
+//     // Menuliskan Hasil
+//     const info = document.querySelector('.info');
+//     info.innerHTML = hasil
+// });
+
+// pKertas.addEventListener('click', function(){
+//     // Menentukan pilihan computer dan player serta hasil
+//     const pilihanComputer = getPilihanComputer();
+//     const pilihanPlayer = pKertas.className;
+//     const hasil = getHasil(pilihanComputer, pilihanPlayer);
+
+//     // Mengubah gambar computer
+//     const imgComputer = document.querySelector('.img-computer');
+//     imgComputer.setAttribute('src', 'img/' + pilihanComputer + '.jpg');
+
+//     // Menuliskan Hasil
+//     const info = document.querySelector('.info');
+//     info.innerHTML = hasil
+// })
+
+
+
+// Mengganti gambar yang ada pada computer
+function putar(){
+    const imgComputer = document.querySelector('.img-computer');
+    const gambar = ['gunting', 'batu', 'kertas'];
+    let i = 0
+    const waktuMulai = new Date().getTime()
+
+    setInterval(function(){
+        if(new Date().getTime() - waktuMulai > 1000){
+            clearInterval;
+            return;
+        }
+        imgComputer.setAttribute('src', 'img/' + gambar[i++] + '.jpg');
+        if(i == gambar.length) i = 0;
+    }, 100)
+}
+
+
+
+// Menentukan cara yang lebih efektif
+const pilihan = document.querySelectorAll('li img');
+pilihan.forEach(function(pil){
+    pil.addEventListener('click', function(){
+    // Menentukan pilihan computer dan player serta hasil
     const pilihanComputer = getPilihanComputer();
-    const pilihanPlayer = pGunting.className;
+    const pilihanPlayer = pil.className;
     const hasil = getHasil(pilihanComputer, pilihanPlayer);
 
-    const imgComputer = document.querySelector('.img-computer')
-    imgComputer.setAttribute('src', 'img/' + pilihanComputer)
-})
+    // Mengacak gambar computer
+    putar()
+
+    setTimeout(function(){
+        // Mengubah gambar computer
+    const imgComputer = document.querySelector('.img-computer');
+    imgComputer.setAttribute('src', 'img/' + pilihanComputer + '.jpg');
+
+    // Menuliskan Hasil
+    const info = document.querySelector('.info');
+    info.innerHTML = hasil
+    }, 1000);
+    });
+    
+});
